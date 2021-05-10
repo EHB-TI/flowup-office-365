49fd15aa61084d420d61f695115e722f747fee53
ArthurDKEHB <58027204+ArthurDKEHB@users.noreply.github.com>
2021-05-10 22:35:02 +0200

    Added TestAPI

--- TestSender/Program.cs
+++ TestSender/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using RabbitMQ.Client;
 using System;
+using System.Data;
 using System.Text;
 using System.Xml;
 using System.Xml.Linq;
@@ -22,9 +23,28 @@ class EmitLogDirect
         //Console.WriteLine($"MySQL version: {version}");
 
         string sql = "SELECT * FROM VoorbeeldTabel";
+        //string testUUID = "5698cd59-3acc-4f15-9ce2-83545cbfe0ba";
+        //string sql = "SELECT * FROM VoorbeeldTabel WHERE uuid = '" + testUUID + "'";
         using var cmd = new MySqlCommand(sql, con);
+        MySqlDataReader rdr = cmd.ExecuteReader();
+        //try
+        //{
+        //    MySqlDataReader rdr = cmd.ExecuteReader();
+        //}
+        //catch(MySqlException ex)
+        //{
+        //    Console.WriteLine(ex.Message);
+        //}
 
-        using MySqlDataReader rdr = cmd.ExecuteReader();
+        //DataTable dt = new DataTable();
+        //MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+        //da.Fill(dt);
+        //int i = Convert.ToInt32(dt.Rows.Count.ToString());
+
+        //if(i == 0)
+        //{
+        //    Console.WriteLine("Failed");
+        //}
 
         while (rdr.Read())
         {
@@ -36,56 +56,58 @@ class EmitLogDirect
 
 
 
+
+
         //var factory = new ConnectionFactory() { HostName = "localhost" };
-        var factory = new ConnectionFactory() { HostName = "10.3.56.6" };
-        using (var connection = factory.CreateConnection())
-        using (var channel = connection.CreateModel())
-        {
-            channel.ExchangeDeclare(exchange: "direct_logs",
-                                    type: "direct");
+        //var factory = new ConnectionFactory() { HostName = "10.3.56.6" };
+        //using (var connection = factory.CreateConnection())
+        //using (var channel = connection.CreateModel())
+        //{
+        //    channel.ExchangeDeclare(exchange: "direct_logs",
+        //                            type: "direct");
 
-            //var severity = (args.Length > 0) ? args[0] : "info";
-            //var message = (args.Length > 1)
-            //              ? string.Join(" ", args.Skip(1).ToArray())
-            //              : "Hello World!";
+        //    //var severity = (args.Length > 0) ? args[0] : "info";
+        //    //var message = (args.Length > 1)
+        //    //              ? string.Join(" ", args.Skip(1).ToArray())
+        //    //              : "Hello World!";
 
-            XmlDocument doc = new XmlDocument();
-            doc.Load("CreateEvent.xml");
+        //    XmlDocument doc = new XmlDocument();
+        //    doc.Load("CreateEvent.xml");
 
-            XmlSchemaSet schema = new XmlSchemaSet();
+        //    XmlSchemaSet schema = new XmlSchemaSet();
 
-            schema.Add("", "EventSchema.xsd");
+        //    schema.Add("", "EventSchema.xsd");
 
-            //XDocument xml = XDocument.Parse(xmlmessage, LoadOptions.SetLineInfo);
-            XDocument xml = XDocument.Parse(doc.OuterXml);
+        //    //XDocument xml = XDocument.Parse(xmlmessage, LoadOptions.SetLineInfo);
+        //    XDocument xml = XDocument.Parse(doc.OuterXml);
 
-            bool xmlValidation = true;
+        //    bool xmlValidation = true;
 
-            xml.Validate(schema, (sender, e) =>
-            {
-                xmlValidation = false;
-            });
+        //    xml.Validate(schema, (sender, e) =>
+        //    {
+        //        xmlValidation = false;
+        //    });
 
-            if (xmlValidation)
-            {
-                Console.WriteLine("XML is geldig");
-            }
-            else
-            {
-                Console.WriteLine("XML is ongeldig");
-            }
+        //    if (xmlValidation)
+        //    {
+        //        Console.WriteLine("XML is geldig");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("XML is ongeldig");
+        //    }
 
-            var severity = "info";
-            string message = doc.InnerXml;
+        //    var severity = "event";
+        //    string message = doc.InnerXml;
 
-            var body = Encoding.UTF8.GetBytes(message);
-            channel.BasicPublish(exchange: "direct_logs",
-                                 routingKey: severity,
-                                 basicProperties: null,
-                                 body: body);
-            //Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
-            Console.WriteLine(message);
-        }
+        //    var body = Encoding.UTF8.GetBytes(message);
+        //    channel.BasicPublish(exchange: "direct_logs",
+        //                         routingKey: severity,
+        //                         basicProperties: null,
+        //                         body: body);
+        //    //Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+        //    Console.WriteLine(message);
+        //}
 
         Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
