a01260997f5d4cfb54d08ba005f60197fee3422c
ArthurDKEHB <58027204+ArthurDKEHB@users.noreply.github.com>
2021-05-29 21:03:06 +0200

    Main_Commit

---
+++ Office_Main/daemon-console/Program1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace UUIDproducer
+{
+    class Program1
+    {
+        //static void Main(string[] args)
+        //{
+
+        //    Consumer.getMessage();
+
+
+        //    //Uuid.createEvent();
+
+
+        //    //Uuid.createMockEvent();
+        //    //Uuid.updateMockEvent();
+        //    //Uuid.updateMockEventFromFrontEnd();
+
+
+        //    /*
+        //    Task task = new Task(() => Producer.send());
+        //    task.Start();
+        //    Consumer.getMessage();
+
+        //    */
+        //}
+    }
+}
