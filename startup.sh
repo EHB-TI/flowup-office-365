da7dd8a66f9547bba5827f2e4e4dbae2f0116f74
Tibo De Munck <tibodemunck@gmail.com>
2021-05-29 22:55:34 +0200

    run docker detached but interactive

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=office-365-main --format="{{.ID}}"))
 docker build . -t office-365-main
-docker run -d office-365-main
+docker run -dit office-365-main
