fceda3daf6da98bde14a30f524d0c3bb2aebfb9f
TiboDeMunck <46536105+TiboDeMunck@users.noreply.github.com>
2021-05-30 16:52:02 +0200

    on startup

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=office-365-main --format="{{.ID}}"))
 docker build . -t office-365-main
-#docker run -dit office-365-main # uncomment this line when you want to your container to start
+#docker run -dit --restart unless-stopped office-365-main # uncomment this line when you want to your container to start
