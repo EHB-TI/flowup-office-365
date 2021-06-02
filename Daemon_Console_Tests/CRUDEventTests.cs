5876e901975fb14c500e02c524fd3e934428f9b7
ArthurDKEHB <58027204+ArthurDKEHB@users.noreply.github.com>
2021-06-02 18:43:19 +0200

    First testing

---
+++ Daemon_Console_Tests/CRUDEventTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UUIDproducer;
+using daemon_console;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Daemon_Console_Tests
+{
+    [TestClass]
+    class CRUDEventTests
+    {
+        [TestMethod]
+        public void Create_Event_Returns_XML_Validation()
+        {
+            // Arrange
+            Producer producer = new Producer();
+            string empty = "";
+
+            // Act
+            string r = producer.getMessage();
+
+
+            // Assert
+            Assert.AreEqual(r, empty, "Account not debited correctly");
+
+        }
+
+        //[TestMethod]
+        //public void Debit_WithValidAmount_UpdatesBalance()
+        //{
+        //    // Arrange
+        //    double beginningBalance = 11.99;
+        //    double debitAmount = 4.55;
+        //    double expected = 7.44;
+        //    BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+        //    // Act
+        //    account.Debit(debitAmount);
+
+        //    // Assert
+        //    double actual = account.Balance;
+        //    Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        //}
+    }
+}
---
+++ daemon-console/TestingDaemon.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace daemon_console
+{
+    public class TestingDaemon
+    {
+        public bool getEventValidation(string inputXML)
+        {
+            bool xmlValidation = true;
+            //bool xmlValidationUser = true;
+            //bool xmlValidationSubscribe = true;
+            //bool xmlValidationError = true;
+
+            XmlDocument xmlDoc = new XmlDocument();
+            XDocument xml = new XDocument();
+
+            xmlDoc.LoadXml(inputXML);
+            xml = XDocument.Parse(xmlDoc.OuterXml);
+            //xsd event validation
+            XmlSchemaSet schema = new XmlSchemaSet();
+            schema.Add("", "EventSchema.xsd");
+
+            XmlSchemaSet schemaSubscribe = new XmlSchemaSet();
+            schemaSubscribe.Add("", "SubscribeSchema.xsd");
+
+            XmlSchemaSet schemaUser = new XmlSchemaSet();
+            schemaUser.Add("", "UserSchema.xsd");
+
+            XmlSchemaSet schemaError = new XmlSchemaSet();
+            schemaError.Add("", "Errorxsd.xsd");
+
+            xml.Validate(schema, (sender, e) =>
+            {
+                xmlValidation = false;
+            });
+
+            //string returnMessage = "";
+            bool returnMessage = xmlValidation;
+
+            return returnMessage;
+        }
+        public bool getUserValidation(string inputXML)
+        {
+            //bool xmlValidation = true;
+            bool xmlValidationUser = true;
+            //bool xmlValidationSubscribe = true;
+            //bool xmlValidationError = true;
+
+            XmlDocument xmlDoc = new XmlDocument();
+            XDocument xml = new XDocument();
+
+            xmlDoc.LoadXml(inputXML);
+            xml = XDocument.Parse(xmlDoc.OuterXml);
+            XmlSchemaSet schemaUser = new XmlSchemaSet();
+            schemaUser.Add("", "UserSchema.xsd");
+
+            xml.Validate(schemaUser, (sender, e) =>
+            {
+                xmlValidationUser = false;
+            });
+
+            //string returnMessage = "";
+            //bool returnMessage = xmlValidationUser;
+
+            //return returnMessage;
+            return xmlValidationUser;
+        }
+    }
+}
